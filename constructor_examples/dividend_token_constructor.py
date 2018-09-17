0c853b64e2a0685c2da82f22703ef643e7bff8c5
karbachinsky <igorkarbachinsky@mail.ru>
2018-09-18 00:05:57 +0300

    update SDK dividend token

--- constructor_examples/dividend_token_constructor.py
+++ constructor_examples/dividend_token_constructor.py
@@ -330,7 +330,7 @@ class Constructor(ConstructorInstance):
 
     # language=Solidity
     _TEMPLATE = """
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
 
 /**
@@ -664,31 +664,32 @@ contract Pausable is Ownable {
 }
 
 
-contract DividendToken is StandardToken {
+contract DividendToken is StandardToken, Ownable {
     event PayDividend(address indexed to, uint256 amount);
-    event Deposit(address indexed sender, uint value);
-    
+    event HangingDividend(address indexed to, uint256 amount) ;
+    event PayHangingDividend(uint256 amount) ;
+    event Deposit(address indexed sender, uint256 value);
+
     /// @dev parameters of an extra token emission
     struct EmissionInfo {
         // new totalSupply after emission happened
-        uint totalSupply;
+        uint256 totalSupply;
 
         // total balance of Ether stored at the contract when emission happened
-        uint totalBalanceWas;
+        uint256 totalBalanceWas;
     }
 
-    function DividendToken()
-    public
+    constructor () public
     {
         m_emissions.push(EmissionInfo({
-            totalSupply: totalSupply_,
+            totalSupply: totalSupply(),
             totalBalanceWas: 0
         }));
     }
 
     function() external payable {
         if (msg.value > 0) {
-            Deposit(msg.sender, msg.value);    
+            emit Deposit(msg.sender, msg.value);
             m_totalDividends = m_totalDividends.add(msg.value);
         }
     }
@@ -696,6 +697,13 @@ contract DividendToken is StandardToken {
     /// @notice Request dividends for current account.
     function requestDividends() public {
         payDividendsTo(msg.sender);
+    }
+
+    /// @notice Request hanging dividends to pwner.
+    function requestHangingDividends() onlyOwner public {
+        owner.transfer(m_totalHangingDividends);
+        emit PayHangingDividend(m_totalHangingDividends);
+        m_totalHangingDividends = 0;
     }
 
     /// @notice hook on standard ERC20#transfer to pay dividends
@@ -714,13 +722,20 @@ contract DividendToken is StandardToken {
 
     /// @dev adds dividends to the account _to
     function payDividendsTo(address _to) internal {
-        var (hasNewDividends, dividends, lastProcessedEmissionNum) = calculateDividendsFor(_to);
+        (bool hasNewDividends, uint256 dividends, uint256 lastProcessedEmissionNum) = calculateDividendsFor(_to);
         if (!hasNewDividends)
             return;
 
         if (0 != dividends) {
-            _to.transfer(dividends);
-            PayDividend(_to, dividends);
+            bool res = _to.send(dividends);
+            if (res) {
+                emit PayDividend(_to, dividends);
+            }
+            else{
+                // _to probably is a contract not able to receive ether
+                emit HangingDividend(_to, dividends);
+                m_totalHangingDividends = m_totalHangingDividends.add(dividends);
+            }
         }
 
         m_lastAccountEmission[_to] = lastProcessedEmissionNum;
@@ -729,21 +744,21 @@ contract DividendToken is StandardToken {
         }
         else {
             m_lastDividents[_to] = m_emissions[lastProcessedEmissionNum.add(1)].totalBalanceWas;
-        }    
+        }
     }
 
     /// @dev calculates dividends for the account _for
     /// @return (true if state has to be updated, dividend amount (could be 0!), lastProcessedEmissionNum)
-    function calculateDividendsFor(address _for) constant internal returns (
+    function calculateDividendsFor(address _for) view internal returns (
         bool hasNewDividends,
-        uint dividends,
-        uint lastProcessedEmissionNum
+        uint256 dividends,
+        uint256 lastProcessedEmissionNum
     ) {
         uint256 lastEmissionNum = getLastEmissionNum();
         uint256 lastAccountEmissionNum = m_lastAccountEmission[_for];
         assert(lastAccountEmissionNum <= lastEmissionNum);
 
-        uint totalBalanceWasWhenLastPay = m_lastDividents[_for];
+        uint256 totalBalanceWasWhenLastPay = m_lastDividents[_for];
 
         assert(m_totalDividends >= totalBalanceWasWhenLastPay);
 
@@ -755,27 +770,26 @@ contract DividendToken is StandardToken {
 
         // if no tokens owned by account
         if (0 == initialBalance)
-            return (false, 0, lastAccountEmissionNum);
+            return (true, 0, lastEmissionNum);
 
         // We start with last processed emission because some ether could be collected before next emission
         // we pay all remaining ether collected and continue with all the next emissions
-        uint iter = 0;
-        uint iterMax = getMaxIterationsForRequestDividends();
+        uint256 iter = 0;
+        uint256 iterMax = getMaxIterationsForRequestDividends();
 
         for (uint256 emissionToProcess = lastAccountEmissionNum; emissionToProcess <= lastEmissionNum; emissionToProcess++) {
             if (iter++ > iterMax)
                 break;
-            
+
             lastAccountEmissionNum = emissionToProcess;
             EmissionInfo storage emission = m_emissions[emissionToProcess];
 
             if (0 == emission.totalSupply)
                 continue;
 
-            uint totalEtherDuringEmission;
+            uint256 totalEtherDuringEmission;
             // last emission we stopped on
             if (emissionToProcess == lastEmissionNum) {
-                totalEtherDuringEmission = 0;
                 totalEtherDuringEmission = m_totalDividends.sub(totalBalanceWasWhenLastPay);
             }
             else {
@@ -784,37 +798,39 @@ contract DividendToken is StandardToken {
             }
 
             uint256 dividend = totalEtherDuringEmission.mul(initialBalance).div(emission.totalSupply);
-
             dividends = dividends.add(dividend);
         }
-        
+
         return (true, dividends, lastAccountEmissionNum);
     }
 
-    function getLastEmissionNum() private constant returns (uint256) {
+    function getLastEmissionNum() private view returns (uint256) {
         return m_emissions.length - 1;
     }
 
     /// @dev to prevent gasLimit problems with many mintings
-    function getMaxIterationsForRequestDividends() internal view returns (uint256) {
+    function getMaxIterationsForRequestDividends() internal pure returns (uint256) {
         return 200;
     }
 
     /// @notice record of issued dividend emissions
-    EmissionInfo[] m_emissions;
+    EmissionInfo[] public m_emissions;
 
     /// @dev for each token holder: last emission (index in m_emissions) which was processed for this holder
-    mapping(address => uint256) m_lastAccountEmission;
+    mapping(address => uint256) public m_lastAccountEmission;
 
     /// @dev for each token holder: last ether balance was when requested dividends
-    mapping(address => uint256) m_lastDividents;
+    mapping(address => uint256) public m_lastDividents;
 
 
-    uint256 m_totalDividends;
+    uint256 public m_totalHangingDividends;
+    uint256 public m_totalDividends;
 }
 
 
 contract MintableDividendToken is DividendToken, MintableToken {
+    event EmissionHappened(uint256 totalSupply, uint256 totalBalanceWas);
+
     function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
         payDividendsTo(_to);
         
@@ -824,7 +840,8 @@ contract MintableDividendToken is DividendToken, MintableToken {
             totalSupply: totalSupply_,
             totalBalanceWas: m_totalDividends
         }));
-        
+
+        emit EmissionHappened(totalSupply(), m_totalDividends);        
         return res;
     }
 }
